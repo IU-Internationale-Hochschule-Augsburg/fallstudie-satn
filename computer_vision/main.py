64f1456c27d83d86b04cc31367e2442302835fc8
Oliver Berndt <oliver.berndt@iu-study.org>
2025-05-10 21:12:59 +0200

    added POST /task endpoint

--- computer_vision/main.py
+++ computer_vision/main.py
@@ -1,15 +1,19 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 from flask import render_template
 import json
-
 from src.Classes.frame_processor import FrameProcessor
+from src.Classes.TaskPipeline.TaskPipeline import TaskPipeline
+from src.Classes.TaskPipeline.TaskForward import TaskForward
+from src.Classes.TaskPipeline.TaskTurn import TaskTurn
 
 app = Flask(__name__)
 camera = FrameProcessor()
 
+
 @app.route('/info')
 def info():
     return json.dumps({'status': 'running'})
+
 
 @app.route('/data')
 def data():
@@ -18,12 +22,13 @@ def data():
             'coord_x': 10,
             'coord_y': 15
         },
-        'vector':{
+        'vector': {
             'dx': 10,
-            'dy':15
+            'dy': 15
         }
     }
     return json.dumps(coordinat_data)
+
 
 @app.route('/videoCapture')
 def video_capture():
@@ -48,9 +53,42 @@ def video_capture():
 
     return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
 @app.route('/liveStream')
 def liveStream():
     return render_template('liveStream.html')
 
+
+@app.route('/task', methods=['POST'])
+def add_task():
+    print("received task")
+    request_body = request.get_json()
+    task_type = request_body.get('type')
+    task = None
+    if task_type == 'forward':
+        duration = request_body.get('duration')
+        if duration is None:
+            task = TaskForward()
+        else:
+            task = TaskForward(duration=duration)
+
+    elif task_type == 'turn':
+        angle = request_body.get('angle')
+        if angle is None:
+            task = TaskTurn()
+        else:
+            task = TaskTurn(angle=angle)
+    else:
+        return Response(status=400, response=f'could not identify task type ${task_type}')
+
+    if TaskPipeline.push_task(task):
+        print("added task", vars(task), "to pipeline")
+    else:
+        print("failed adding task", vars(task), "to pipeline")
+        return Response(status=500)
+
+    return Response(status=200)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000, threaded=True)
+    app.run(host='0.0.0.0', port=5000, threaded=True)

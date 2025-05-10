05a0ea05d30dc1d10850866202502020e7e8142e
Oliver Berndt <oliver.berndt@iu-study.org>
2025-05-10 16:34:05 +0200

    feat(issue-57): added init and to_json for TaskForward class

--- computer_vision/src/Classes/TaskPipeline/TaskForward.py
+++ computer_vision/src/Classes/TaskPipeline/TaskForward.py
@@ -1,9 +1,17 @@
 from Task import Task
 
 class TaskForward(Task):
+    """
+    This class represent a task to drive forward
+    """
 
-    def __init__(self):
-        pass
+    def __init__(self, duration:int = 1000):
+        """
+        :param duration: how long to drive forward in milliseconds, defaults to 1000
+        :type duration: int, optional
+        """
+        self.type:str = "forward"
+        self.duration:int = duration
     
-    def to_json():
-        print("hi")
+    def to_json(self):
+        return vars(self)

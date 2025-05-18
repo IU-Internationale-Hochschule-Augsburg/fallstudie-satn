093e47b1dc66e1917796a92b21f574c038816cb6
SpaRaw <91481370+SpaRaw@users.noreply.github.com>
2025-05-18 18:44:25 +0200

    feat(#72): add main component for roboter

---
+++ roboter/main/main.ino
@@ -0,0 +1,15 @@
+#include <Zumo32U4.h>
+#include "functions.h"
+
+Zumo32U4Motors motors;
+Zumo32U4Encoders encoders;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

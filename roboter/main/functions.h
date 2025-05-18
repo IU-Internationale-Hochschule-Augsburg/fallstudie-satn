e7607b524ee7700ea816bb10bd196fb6babe8c31
SpaRaw <91481370+SpaRaw@users.noreply.github.com>
2025-05-18 18:48:52 +0200

    feat(#72): refactor calibration code

--- roboter/main/functions.h
+++ roboter/main/functions.h
@@ -39,4 +39,68 @@ void turnOnSpot(int angle) {
   motors.setRightSpeed(0);  // Stoppe rechten Motor
   
 }
+
+void calibrate() {
+  // Make sure Zumo stands still
+  motors.setSpeeds(0,0);
+
+  // todo: Send request to /posistion and /direction endpoint
+  // starting postion
+  float x1 = 0.0;
+  float y1 = 0.0;
+  float dir1 = 0.0;
+
+  // drive forward
+  motors.setSpeeds(200, 200);
+  delay(1000);
+  motors.setSpeeds(0,0);
+
+  // todo: Send request to /posistion and /direction endpoint
+  // ending postion
+  float x2 = 0.0;
+  float y2 = 0.0;
+  float dir2 = 0.0;
+
+
+  float deltaAngle = fabs(dir2 - dir1)
+  if (deltaAngle > 180) {
+    deltaAngle = 360 - deltaAngle;
+  }
+  if (deltaAngle == 0) {
+    return true;
+  }
+
+  bool turnLeft;
+  if (
+      (
+       dir1 < 180 &&
+       (dir2 < dir1 || dir2 > dir1 + 180)
+      )||(
+       dir1 > 180 && dir2 < dir1 && dir2 > dir1 - 180
+      )
+  ) {
+    turnLeft = true;
+  } else {
+    turnLeft = false;
+  }
+
+  float dx = x2 - x1;
+  float dy = y2 - y1;
+  float directLength = sqrt((dx * dx) + (dy * dy));
+
+  float theta = deltaAngle * PI / 180;
+  float radius = directLength / (2 * sin(theta / 2));
+  float radius1 = radius - 1; //replace 1 with half of distance between chains
+  float radius2 = radius + 1;
+
+  float length1 = radius1 * theta
+  float length2 = radius2 * theta;
+  float adjustment = length1 / length2;
+  if (turnLeft) {
+    // adjust right
+  } else {
+    //adjust left
+  }
+  // todo: set adjustment as environment variable
+}
 #endif 

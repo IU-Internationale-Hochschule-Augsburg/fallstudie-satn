d25bd559c8732515971b453d41e99c3b6bc5ed54
sparaw <sparaw@Laptop-Korbinian.>
2025-06-03 14:10:45 +0200

    realisticly simulate video stream

--- computer_vision/src/Classes/ObjectDetection/ObjectDetection.py
+++ computer_vision/src/Classes/ObjectDetection/ObjectDetection.py
@@ -79,7 +79,7 @@ class ObjectDetection:
             'dy': h1
         }
 
-    def get_object_position(self, img, t=100, min_area=250, only_contours=False):
+    def get_object_position(self, img, t=100, min_area=300, only_contours=False):
         """
         Detects all objects in the image that are not part of the Zumo robot.
 
@@ -92,15 +92,14 @@ class ObjectDetection:
         Returns:
             list: Object position dictionaries or contours.
         """
-        print("inside get_object_position")
+
         # Get Zumo position to exclude it from object detection
         zumo_data = self.get_zumo_position(img)
-        print("zumo_data", zumo_data)
-        _, tresh = cv2.threshold(img, t, 180, cv2.THRESH_BINARY_INV)
+        _, tresh = cv2.threshold(img, t, 190, cv2.THRESH_BINARY_INV)
 
         # Detect contours
         contours, _ = cv2.findContours(tresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        print("contours", contours)
+
         if zumo_data is not None:
             zumo_x, zumo_y, zumo_w, zumo_h = zumo_data.values()
 
@@ -123,10 +122,10 @@ class ObjectDetection:
                 'dx': w,
                 'dy': h
             })
-
+        print("objects", objects)
         return objects
 
-    def crop_image(self, gray_img, dark_thresh=80):
+    def crop_image(self, gray_img, dark_thresh=90):
         """
         Crops the main region of interest (ROI) based on dark borders and edge detection.
 
@@ -208,19 +207,12 @@ class ObjectDetection:
     def handle_object_detection_from_source(self):
         print("handle object detection from source")
         ok, gray_frame = self.camera.get_frame()  # get_frame gibt JPEG-Bytes zurück
-        print("is_ok", ok)
-        print("gray_frame", gray_frame)
-
-        if not ok or gray_frame is None:
-            print("poll img")
-            while not ok:
-                ok, gray_frame = camera.get_frame()
-            print("poll finished")
 
         cropped = self.crop_image(gray_frame)
-        print(cropped.shape)
+
         obj_pos = self.get_object_position(cropped)
         zumo_pos = self.get_zumo_position(cropped)
+
         print(obj_pos)
         print(zumo_pos)
         self.camera.release()

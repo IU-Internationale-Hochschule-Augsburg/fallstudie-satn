72c1c5942544765921f74450677852ce20f93825
Oliver Berndt <oliver.berndt@iu-study.org>
2025-06-03 10:19:10 +0200

    fixed TaskPipeline tests

--- computer_vision/src/Classes/TaskPipeline/TaskPipeline.py
+++ computer_vision/src/Classes/TaskPipeline/TaskPipeline.py
@@ -2,16 +2,18 @@ from src.Classes.ObjectDetection.ObjectDetection import ObjectDetection
 from src.Classes.TaskPipeline.Task import Task
 from src.Classes.TaskPipeline.TaskList import TaskList
 from src.Utils.pathfinding import get_next_task
+from typing import Optional
 
 
 class TaskPipeline():
     """
     High-level manager for a sequence of Tasks, delegating storage responsibilities to TaskList.
     """
+
     def __init__(self):
         # Create a new TaskList to hold and persist tasks
         self.taskList = TaskList()
-    
+
     def push_task(self, task: Task) -> bool:
         """
         Add a new task to the pipeline.
@@ -21,7 +23,7 @@ class TaskPipeline():
         """
         return self.taskList.push_task(task)
 
-    def pop_task(self) -> Task:
+    def pop_task(self) -> Optional[Task]:
         """
         Retrieve and remove the most recent task from the pipeline.
 
@@ -31,5 +33,7 @@ class TaskPipeline():
         if retrieval is None:
             od = ObjectDetection()
             objects = od.handle_object_detection_from_source()
-            retrieval = get_next_task(objects)
+            if objects is None:
+                return None
+            retrieval: Task = get_next_task(objects)
         return retrieval
--- computer_vision/unit_tests/TaskPipeline_test.py
+++ computer_vision/unit_tests/TaskPipeline_test.py
@@ -1,7 +1,11 @@
 from MockTask import MockTask
 import unittest
+from unittest.mock import patch
 import os
+from src.Classes.ObjectDetection.ObjectDetection import ObjectDetection
+from src.Classes.TaskPipeline.TaskForward import TaskForward
 from src.Classes.TaskPipeline.TaskPipeline import TaskPipeline
+import src.Utils.pathfinding
 
 class TestTaskPipeline(unittest.TestCase):
 
@@ -20,6 +24,19 @@ class TestTaskPipeline(unittest.TestCase):
         popped = pipeline.pop_task()
         self.assertEqual(task, popped)
 
-    def test_pipeline_pop_empty(self):
+    @patch('src.Classes.TaskPipeline.TaskPipeline.get_next_task', return_value=TaskForward())
+    @patch.object(ObjectDetection, 'handle_object_detection_from_source', return_value={})
+    def test_pipeline_pop_empty(self, mock_od, mock_get_task):
+        expected_result = TaskForward()
         pipeline = TaskPipeline()
-        self.assertIsNone(pipeline.pop_task())
+        result = pipeline.pop_task()
+        mock_get_task.assert_called_with({})
+        mock_od.assert_called_once()
+        self.assertEqual(vars(expected_result), vars(result))
+
+    @patch.object(ObjectDetection, 'handle_object_detection_from_source', return_value=None)
+    def test_pipeline_pop_empty_od_fail(self, mock_od):
+        pipeline = TaskPipeline()
+        result = pipeline.pop_task()
+        mock_od.assert_called_once()
+        self.assertIsNone(result)
